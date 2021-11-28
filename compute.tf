b5d4dd833196761187413999d70df1da449e7167
al9988 <54456219+al9988@users.noreply.github.com>
2021-11-28 12:38:33 -0700

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -49,7 +49,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
   size                  = "Standard_DS1_v2"
 
   os_disk {
-    name                 = "AlanLOsDisk"
+    name                 = "myOsDisk"
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
   }
